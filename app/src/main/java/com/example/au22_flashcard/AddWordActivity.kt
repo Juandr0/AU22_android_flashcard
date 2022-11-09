859f3f36914b5785d58f2a9c5ff663a53a35a809
Alex <alexander.cg.carlsson@gmail.com>
2022-11-09 17:03:35 +0100

    initialize db to activity

--- app/src/main/java/com/example/au22_flashcard/AddWordActivity.kt
+++ app/src/main/java/com/example/au22_flashcard/AddWordActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.room.Database
 
 class AddWordActivity : AppCompatActivity() {
 
@@ -11,6 +12,7 @@ class AddWordActivity : AppCompatActivity() {
     lateinit var backBtn : Button
     lateinit var enEditText : EditText
     lateinit var svEditText : EditText
+    lateinit var db : AppDatabase
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +20,25 @@ class AddWordActivity : AppCompatActivity() {
         backBtn = findViewById(R.id.addWord_backButton)
         enEditText = findViewById(R.id.addWord_englishEditText)
         svEditText = findViewById(R.id.addWord_swedishEditText)
+        db = AppDatabase.getInstance(this)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_word)
+
+        backBtn.setOnClickListener {
+            finish()
+        }
+
+        saveBtn.setOnClickListener {
+            saveWordToDb()
+        }
+    }
+
+
+    private fun saveWordToDb(){
+        var englishWord = enEditText.text.toString()
+        var swedishWord = svEditText.text.toString()
+
+        // lägg in i DB
     }
 }
