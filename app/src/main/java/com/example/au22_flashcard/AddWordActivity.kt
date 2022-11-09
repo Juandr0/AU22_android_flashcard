dfca85a298dbecab248c15b6946017ab288045c6
Alex <alexander.cg.carlsson@gmail.com>
2022-11-09 17:01:17 +0100

    initialized views

--- app/src/main/java/com/example/au22_flashcard/AddWordActivity.kt
+++ app/src/main/java/com/example/au22_flashcard/AddWordActivity.kt
@@ -2,9 +2,23 @@ package com.example.au22_flashcard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class AddWordActivity : AppCompatActivity() {
+
+    lateinit var saveBtn : Button
+    lateinit var backBtn : Button
+    lateinit var enEditText : EditText
+    lateinit var svEditText : EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        saveBtn = findViewById(R.id.addWord_saveButton)
+        backBtn = findViewById(R.id.addWord_backButton)
+        enEditText = findViewById(R.id.addWord_englishEditText)
+        svEditText = findViewById(R.id.addWord_swedishEditText)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_word)
     }
